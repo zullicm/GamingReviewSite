0038714a1ed2ef81183af64702c1a7d396d49186
Christopher Marzulli <chriszulli15@gmail.com>
2022-12-03 13:17:17 -0500

    added side review on homepage

--- client/src/Components/ReviewCard.js
+++ client/src/Components/ReviewCard.js
@@ -3,11 +3,12 @@ import React from "react";
 function ReviewCard({ review }){
   
   return(
-    
-    <div className="review-container" >
-      <p className="review-username"><b>{review.user.username}</b></p>
-      <p>{review.comment}</p>
-      
+    <div className="">
+      <div className="review-container" >
+        <p className="review-username"><b>{review.user.username}</b> said:<br/> and gave it a rating of:
+        <br/><b>{review.rating}</b></p>
+        <p>{review.comment}</p>
+      </div>
     </div>
   )
 }
